22dc4d4ef0af3901878098ce1b7b748b3a615dca
Md Shihab uddin <166051075+Shihab1315@users.noreply.github.com>
2025-11-19 18:12:15 +0600

    Add files via upload

---
+++ A. Ichihime and Triangle.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+int t;
+cin>>t;
+ while(t--){
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<a<<" "<<c<<" "<<c<<'\n';
+ }
+    return 0;
+}
