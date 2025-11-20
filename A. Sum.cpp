b61ab7d26bc1460fa46b0132c07d1b86086e0915
Md Shihab uddin <166051075+Shihab1315@users.noreply.github.com>
2025-11-20 09:53:41 +0600

    Add files via upload

--- A. Sum.cpp
+++ A. Sum.cpp
@@ -1,23 +1,29 @@
 
 #include <bits/stdc++.h>
-
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define ll long long
 using namespace std;
 
 int main()
 {
 ios::sync_with_stdio(0);
 cin.tie(0);
-int t;
-cin>>t;
-while (t--)
-{
-   int a,b,c;
-   cin>>a>>b>>c;
-   if(a+b==c || a+c==b || b+c==a ){
-    cout<<"YES"<<endl;
-   }else{
-    cout<<"NO"<<endl;
-   }
-}
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        int x=a+b;
+        int y=a+c;
+        int z=b+c;
+        if(x==c || y==b || z==a){
+            yes;
+        }else{
+            no;
+        }
+    }
+    
     return 0;
 }
