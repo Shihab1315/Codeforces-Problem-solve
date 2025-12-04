15b16a00dac5957a3c8b25afb1c217d332bd6ace
Md Shihab uddin <166051075+Shihab1315@users.noreply.github.com>
2025-12-04 20:43:52 +0600

    Add files via upload

---
+++ A. GCD vs LCM.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+     int t;
+     cin>>t;
+     while (t--)
+     {
+        int n;
+        cin>>n;
+        int a=n-3;
+        cout<<a<<" "<<1<<" "<<1<<" "<<1<<'\n';
+     }
+     
+    return 0;
+}
