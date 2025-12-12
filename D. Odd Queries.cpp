72aa8484eb70f0c0c025646ce1c75be0b946ba56
Md Shihab uddin <166051075+Shihab1315@users.noreply.github.com>
2025-12-12 08:51:17 +0600

    Add files via upload

---
+++ D. Odd Queries.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define ll long long
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n, k;
+        cin >> n >> k;
+
+        vector<ll> v(n), prefix(n);
+        for(int i = 0; i < n; i++){
+            cin >> v[i];
+        }
+
+        prefix[0] = v[0];
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i-1] + v[i];
+        }
+
+        while(k--){
+            ll a, b, c;
+            cin >> a >> b >> c;
+
+            a--, b--; 
+
+          
+            ll ans = (a > 0 ? prefix[a-1] : 0);
+
+           
+            if(b+1 < n) ans += prefix[n-1] - prefix[b];
+
+           
+            ll x = b - a + 1;
+
+            
+            ans += c * x;
+
+            if(ans % 2 == 1) yes;
+            else no;
+        }
+    }
+    return 0;
+}
