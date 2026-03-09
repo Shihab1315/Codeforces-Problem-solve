0c05093b8c95c4fab1e5879bc3072ffc9f306089
Md Shihab uddin <166051075+Shihab1315@users.noreply.github.com>
2026-03-09 15:18:30 +0600

    Add files via upload

---
+++ A. Gregor and Cryptography.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+       int n;
+       cin>>n;
+      
+       
+       cout<<2<<" "<<n-1<<'\n';
+    }
+    
+    return 0;
+}
