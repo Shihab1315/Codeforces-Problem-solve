55cfc8f5c1a1064f1c86a1c990c22a75fc655ae1
Md Shihab uddin <166051075+Shihab1315@users.noreply.github.com>
2026-04-11 08:40:02 +0600

    Add files via upload

---
+++ A. The 67th Integer Problem.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+    int y;
+    cin>>y;
+    while (y--)
+    {
+        int number1;
+        cin>>number1;
+        cout<<number1<<'\n';
+    }
+    
+    return 0;
+}
