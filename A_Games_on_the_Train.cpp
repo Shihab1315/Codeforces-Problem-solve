bcdfaf482d6d71a8de0679f34f20fa4621081b5a
Md Shihab uddin <166051075+Shihab1315@users.noreply.github.com>
2026-07-18 19:28:13 +0600

    Add files via upload

---
+++ A_Games_on_the_Train.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+   int t;
+   cin>>t;
+   while(t--){
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    int mx=INT_MIN,mn=INT_MAX;
+    for(int i=0;i<n;i++){
+          cin>>v[i];
+          mx=max(mx,v[i]);
+          mn=min(mn,v[i]);
+    }
+    cout<<mx-mn+1<<'\n';
+   }
+    return 0;
+}
